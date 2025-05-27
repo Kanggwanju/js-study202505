a3953693e076cb93a0344b93926dc1e4c4b64900
Kanggwanju <xprtktm1@gmail.com>
2025-05-27 10:56:37 +0900

    js - 변수와 데이터타입 - 숫자

---
+++ 1-2-variable/1-variable.js
@@ -0,0 +1,48 @@
+
+let apple = 3 * 5;
+console.log(apple +3);
+
+let myName = "파스타";
+apple =100;
+console.log(apple +3);
+console.log(apple);
+console.log(myName + 3);
+
+// 변수의 이름을 지을 때 주의사항
+let friendName3; // 관례: camel case
+// let 3friendName; 이름이 숫자로 시작하면 안됨
+let $friendName_; // 특수문자 사용불가 : _, $
+// let let; // 예약어(키워드)는 변수이름 사용 불가
+
+
+// 변수의 선언
+let banana;
+let Banana; // 대소문자를 다르게하면 다른 변수다
+
+// 변수의 초기화 (initialize)
+banana = "맛도리 뻐네너";
+
+// 만약 변수를 초기화하지 않으면 undefined가 저장됨.
+console.log(Banana);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 1-2-variable/2-scope.js
@@ -0,0 +1,28 @@
+let number = 10;
+let banana = 0;
+// 변수는 중복으로 선언 불가능
+// let number = 20;
+
+// 블록 레벨 스코프를 지원
+if (true) {
+    let number = 30;
+    console.log(`number in if : ${number}`);
+}
+
+console.log(`number in if : ${number}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
