f435430775553ebbb94fbf01715efa2cc5afac2b
Kanggwanju <xprtktm1@gmail.com>
2025-06-05 10:25:53 +0900

    함수 - let, const

---
+++ 2-3-function/7-const.js
@@ -0,0 +1,18 @@
+
+const x = 10;
+// x = 20;
+console.log(`x: ${x}`);
+
+let numbers = [10, 20, 30, 40];
+// let copyNumbers = [];
+// let copyNumbers = numbers.slice(); // 전체 복사
+let copyNumbers = [...numbers];
+// for (const n of numbers) {
+//   copyNumbers.push(n);
+// }
+
+numbers[1] = 99;
+copyNumbers[2] = 777;
+
+console.log('numbers:', numbers);
+console.log('copyNumbers:', copyNumbers);
