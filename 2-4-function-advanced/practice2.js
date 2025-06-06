ad6805918c0a367c4467241aa4b82975e0ae45b8
Kanggwanju <xprtktm1@gmail.com>
2025-06-06 16:12:11 +0900

    함수 고급 - 콜백 함수 자체 실습 (gpt 출제) - some

--- 2-4-function-advanced/practice2.js
+++ 2-4-function-advanced/practice2.js
@@ -41,14 +41,21 @@ function every(callback) {
   return true;
 }
 
+function some(callback) {
+  for (const product of productList) {
+    if (callback(product)) return true;
+  }
+  return false;
+}
+
 
 const result1 = every(product => product.stock > 0);
 console.log(result1);  // false
 
-/*
 const result2 = some(product => product.category === '음료');
 console.log(result2);  // true
 
+/*
 const result3 = none(product => product.name.endsWith('이'));
 console.log(result3);  // false
 
