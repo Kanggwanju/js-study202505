a9330be7bdbb6ed39895575992a064f7caf4ce31
Kanggwanju <xprtktm1@gmail.com>
2025-06-09 12:53:01 +0900

    배열 고차함수 : map, filter - 해설

--- 2-6-array-high-order-func/practice1.js
+++ 2-6-array-high-order-func/practice1.js
@@ -77,37 +77,34 @@ const traders = [
 // 연습 1: 2022년에 발생한 모든 거래를 찾아
 //   거래자 정보(이름, 도시)를 배열에 매핑해주세요
 // [ {name, city}, {name, city}, ... ]
-const finalUserList1 = traders
+const traderIn2022 = traders
   .filter(trade => trade.year === 2022)
   .map(trade => ({
     name: trade.trader.name,
     city: trade.trader.city
 }));
-console.log(finalUserList1);
+console.log(traderIn2022);
 console.log('=======================================');
 
 // 연습 2: 거래자가 근무하는 모든 도시이름을 배열에 담아주세요.
 const finalCityList2 = traders.map(trade => trade.trader.city);
-// console.log(finalCityList2);
 const distinctCities2 = [...new Set(finalCityList2)];
 console.log(distinctCities2);
 console.log('=======================================');
 
 // 연습 3: 대전에 근무하는
 // 모든 거래자를 찾아 거래자정보(이름, 도시)를 배열에 매핑해주세요.
-const finalUserList3 = traders
+const traderInDaejeon = traders
   .filter(trade => trade.trader.city === '대전')
-  .map(trade => ({
-    name: trade.trader.name,
-    city: trade.trader.city
-}));
-console.log(finalUserList3);
+  .map(trade => trade.trader);
+
+console.log(traderInDaejeon);
 console.log('=======================================');
 
 // 연습 4: 모든 거래자의 이름을 배열에 모아주세요.
-const finalUserList4 = traders.map(trade => trade.trader.name);
-console.log(finalUserList4);
-const distinctNames4 = [...new Set(finalUserList4)];
+const allTraderNames = traders.map(trade => trade.trader.name);
+console.log(allTraderNames);
+const distinctNames4 = [...new Set(allTraderNames)];
 console.log(distinctNames4);
 console.log('=======================================');
 
