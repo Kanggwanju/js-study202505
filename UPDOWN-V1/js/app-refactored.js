0edd8cd65299e5c1ffa2e3288491d96237d2e5a0
Kanggwanju <xprtktm1@gmail.com>
2025-06-18 10:26:19 +0900

    UP & DOWN 게임 실습 풀이 V1

--- UPDOWN-V1/js/app-refactored.js
+++ UPDOWN-V1/js/app-refactored.js
@@ -39,11 +39,7 @@ function updateFeedback(feedbackText, feedbackClass) {
 function judgeGuess() {
   const { secretNumber, userAnswer } = gameData;
 
-  // GAME OVER 처리
-  if (gameData.remainingChanges === 0) {
-    showFinishModal(false);
-    return;
-  }
+
 
 
 
@@ -88,6 +84,11 @@ function judgeGuess() {
     ) {
       updateFeedback(`정답은 ${gameData.minRange}밖에 없네요!`, 'correct');
       showFinishModal();
+    }
+
+    // GAME OVER 처리
+    if (gameData.remainingChanges === 0) {
+      showFinishModal(false);
     }
 
     // 모든 판정이 끝난 후 UI 업데이트
