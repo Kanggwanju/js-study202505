fdcd05bc56ae86ae04060ba58e658a5091c3fb24
Kanggwanju <xprtktm1@gmail.com>
2025-06-20 14:21:02 +0900

    모듈 쪼개기 export import

--- 7-1-module/app.js
+++ 7-1-module/app.js
@@ -1,13 +1,30 @@
 
+import { $btn } from './dom.js';
+import { clickHandler } from './event-handler.js';
+
+// import { add as plus, sub, multi } from './calculation.js'
+// import { operation } from './calculation.js'
+import xx from './calculation.js';
+
+// const {add: plus, sub, multi} = operation;
+const {add: plus, sub, multi} = xx;
+
+const add = (...numbers) => {
+  return numbers.reduce((acc, cur) => acc + cur, 0);
+};
+
+console.log(plus(10, 20));
+console.log(sub(10, 8));
+console.log(multi(3, 5));
+
+
 // 모든 태그가 그려진 이후에 발생하는 이벤트
 // document.addEventListener('DOMContentLoaded', e => {
 
-const $btn = document.getElementById('btn');
+
 console.log('btn: ', $btn);
 
-const clickHandler = e => {
-  alert('안뇽!');
-};
+
 
 $btn.addEventListener('click', clickHandler);
 
